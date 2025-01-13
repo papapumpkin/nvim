effbd2a12c04e10b0e9a6e2acfdc0562eb26ecf7
Aaron <aa11salm@gmail.com>
2025-01-12 19:53:30 -0500

    configure tree and show hidden files

---
+++ lua/beanlab/plugins/nvim-tree.lua
@@ -0,0 +1,22 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons"
+    },
+    config = function()
+        require("nvim-tree").setup({
+            sort_by = "case_sensitive",
+            view = {
+                width = 30,
+            },
+            filters = {
+                dotfiles = false,
+            },
+        })
+        
+        -- Set keymaps for opening/closing tree
+        vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>', {silent = true})
+        -- or use <leader>e if you prefer
+        -- vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', {silent = true})
+    end
+}
