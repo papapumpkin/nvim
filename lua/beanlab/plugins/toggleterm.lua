c09f94f7dbd7fee097adaa502389e0680cdaefe9
Aaron <aa11salm@gmail.com>
2025-01-14 20:36:11 -0500

    horizontal terminal window

--- lua/beanlab/plugins/toggleterm.lua
+++ lua/beanlab/plugins/toggleterm.lua
@@ -1,23 +1,21 @@
 -- ~/nvim/lua/beanlab/plugins/toggleterm.lua
 
 return {
-  'akinsho/toggleterm.nvim',
-  version = "*",
-  config = function()
-    require("toggleterm").setup({
-        size = 10,
-        open_mapping = [[<F7>]],
-        shading_factor = 2,
-        direction = "float",
-        float_opts = {
-            border = "curved",
-            highlights = {
-                border = "Normal",
-                background = "Normal",
-            },
-        },
-    }) 
-
-  end,
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+		require("toggleterm").setup({
+			size = 10,
+			open_mapping = [[<F7>]],
+			shading_factor = 2,
+			direction = "horizontal",
+			float_opts = {
+				border = "curved",
+				highlights = {
+					border = "Normal",
+					background = "Normal",
+				},
+			},
+		})
+	end,
 }
-
