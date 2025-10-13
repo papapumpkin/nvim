1bebe4dcc6c235df33264123ccc65d3beeb10ee0
Aaron Salm <aaron.salm@noetik.ai>
2025-10-13 17:14:56 -0400

    fix wonky git keymaps

--- lua/beanlab/plugins/fugitive.lua
+++ lua/beanlab/plugins/fugitive.lua
@@ -6,27 +6,27 @@ return {
 		vim.keymap.set("n", "<leader>gc", "<CMD>Git commit -v<CR>", { desc = "Git commit" })
 		vim.keymap.set("n", "<leader>gp", "<CMD>Git push<CR>", { desc = "Git push" })
 		vim.keymap.set("n", "<leader>gP", "<CMD>Git pull<CR>", { desc = "Git pull" })
-		
+
 		-- Git restore - restore current file
 		vim.keymap.set("n", "<leader>gr", "<CMD>Git restore %<CR>", { desc = "Git restore current file" })
-		
+
 		-- Git add - stage current file or all files
 		vim.keymap.set("n", "<leader>ga", "<CMD>Git add %<CR>", { desc = "Git add current file" })
 		vim.keymap.set("n", "<leader>gA", "<CMD>Git add .<CR>", { desc = "Git add all" })
-		
+
 		-- Git diff
 		vim.keymap.set("n", "<leader>gd", "<CMD>Gdiffsplit<CR>", { desc = "Git diff current file" })
-		
-		-- Git blame
-		vim.keymap.set("n", "<leader>gb", "<CMD>Git blame<CR>", { desc = "Git blame" })
-		
+
 		-- Git log
 		vim.keymap.set("n", "<leader>gl", "<CMD>Git log<CR>", { desc = "Git log" })
 		vim.keymap.set("n", "<leader>gL", "<CMD>Git log --oneline --graph --all<CR>", { desc = "Git log graph" })
 
-		-- Git checkout - more intuitive mnemonics
-		vim.keymap.set("n", "<leader>gcom", "<CMD>Git checkout main<CR>", { desc = "Git checkout main" })
-		vim.keymap.set("n", "<leader>gcob", function()
+ 	-- Git blame
+ 	vim.keymap.set("n", "<leader>gB", "<CMD>Git blame<CR>", { desc = "Git blame" })
+ 
+ 	-- Git checkout and branch
+ 	vim.keymap.set("n", "<leader>gm", "<CMD>Git checkout main<CR>", { desc = "Git checkout main" })
+ 	vim.keymap.set("n", "<leader>gb", function()
 			local branch = vim.fn.input("Branch name: ")
 			if branch ~= "" then
 				vim.cmd("Git checkout " .. branch)
